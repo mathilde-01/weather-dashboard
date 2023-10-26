266a518cb3285d71c61bc6e0e43bf9d6533edb8d
mathilde-01 <mathilde.7892@gmail.com>
2023-10-26 14:47:33 -0400

    renamed file

--- _assets/script.js
+++ _assets/script.js
@@ -1,3 +1,5 @@
+console.log('script', 'connected')
+
 function weatherApi() {
   var weatherUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=5baa3eb3f1fb49cff1d20f45856a9452";
 
