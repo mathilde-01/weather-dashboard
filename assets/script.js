a175145f3afa51c955b2c925f2b697dc0f44ac74
mathilde-01 <mathilde.7892@gmail.com>
2023-10-27 16:14:10 -0400

    errors

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var ApiKey = "5baa3eb3f1fb49cff1d20f45856a9452";
 var cityName = "";
 var fetchButton = document.getElementById("fetch-button");
 var cityBox = document.querySelector(".boxCity");
+var lat = "";
+var lon = "";
 
 function weatherApi(cityName) {
   var weatherUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${ApiKey}`;
@@ -14,8 +16,8 @@ function weatherApi(cityName) {
     })
 
     .then(function (data) {
-      var lat = data[0].lat;
-      var lon = data[0].lon;
+      lat = data[0].lat;
+      lon = data[0].lon;
       console.log(lat, lon);
       return fetchCurrent(lat, lon);
       // .then(function () { // this should be in fetchCurrent
@@ -38,51 +40,49 @@ function fetchCurrent(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      console.log(data);
       // current city
-      var currentCityInput = $("#inputCity").val("")
-      var currentCityEl = document.getElementById("inputCity")
-      console.log(currentCityInput)
-      currentCityEl.textContent = currentCityInput
+      var currentCityInput = $("#inputCity").val("");
+      var currentCityEl = document.getElementById("inputCity");
+      console.log(currentCityInput);
+      currentCityEl.textContent = currentCityInput;
       // create current day
-      var currentDay = moment().format("M/D/YYYY")
-      console.log(currentDay)
-      var day = document.createElement('h2')
-      day.textContent = currentDay
-      cityBox.appendChild(day)
+      var currentDay = moment().format("M/D/YYYY");
+      console.log(currentDay);
+      var day = document.createElement("h2");
+      day.textContent = currentDay;
+      cityBox.appendChild(day);
       // create current temp
-      var temp = document.createElement('p')
-      temp.textContent = `\n`+`Temp:${data.main.temp}`
-      cityBox.appendChild(temp)
-      // create current wind 
-      var wind = document.createElement('p')
-      wind.textContent = `\n`+`Wind:${data.wind.speed}` + `MPH`
-      cityBox.appendChild(wind)
+      var temp = document.createElement("p");
+      temp.textContent = `\n` + `Temp:${data.main.temp}`;
+      cityBox.appendChild(temp);
+      // create current wind
+      var wind = document.createElement("p");
+      wind.textContent = `\n` + `Wind:${data.wind.speed}` + `MPH`;
+      cityBox.appendChild(wind);
       // create current humidity
-      var humid = document.createElement('p')
-      humid.textContent = `\n`+`Humidity:${data.main.humidity}`+ `%`
-      cityBox.appendChild(humid)
-    
+      var humid = document.createElement("p");
+      humid.textContent = `\n` + `Humidity:${data.main.humidity}` + `%`;
+      cityBox.appendChild(humid);
     });
- 
 }
 
 // called when the search form is submitted
-$(".form").on("submit", function() {
+$(".form").on("submit", function () {
   event.preventDefault();
-  
+
   // get name of city searched
   var cityName = $("#inputCity").val();
 
   if (cityName === "" || cityName == null) {
-      // send alert if search input is empty when submitted
-      alert("Please enter name of city.");
-      event.preventDefault();
+    // send alert if search input is empty when submitted
+    alert("Please enter name of city.");
+    event.preventDefault();
   } else {
-      // if cityName is valid, add it to search history list and display its weather conditions
-      fetchCurrent(cityName);
-      fetchForecast(cityName);
-      weatherApi(cityName);
+    // if cityName is valid, add it to search history list and display its weather conditions
+    weatherApi(cityName);
+    fetchForecast(cityName);
+    //local Storage code
   }
 });
 
@@ -100,9 +100,9 @@ function fetchForecast(lat, lon) {
       console.log(data);
       for (var i = 0; i < data.list.length; i = i + 8) {
         console.log(data.list[i]);
-        var temp = document.createElement('p')
-      temp.textContent = `\n`+`Temp:${data.main.temp}`
-      cityBox.appendChild(temp)
+        var temp = document.createElement("p");
+        temp.textContent = `\n` + `Temp:${data.main.temp}`;
+        cityBox.appendChild(temp);
       }
     });
 }
