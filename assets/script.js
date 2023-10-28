7123be83e94408b1813a5d25fe8779d3151d3909
mathilde-01 <mathilde.7892@gmail.com>
2023-10-28 16:57:54 -0400

    added 5day forecast and icons

--- assets/script.js
+++ assets/script.js
@@ -42,15 +42,22 @@ function fetchCurrent(lat, lon) {
     .then(function (data) {
       console.log(data);
       // current city
-      var currentCity = document.createElement("h2");
+      cityBox.innerHTML = "";
+      var currentCity = document.createElement("h6");
       currentCity.textContent = `${data.name}`;
       cityBox.appendChild(currentCity);
       // create current day
       var currentDay = moment().format("M/D/YYYY");
       console.log(currentDay);
-      var day = document.createElement("h2");
+      var day = document.createElement("p");
       day.textContent = currentDay;
       cityBox.appendChild(day);
+      var imgIcon = document.createElement("img");
+      imgIcon.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      );
+      cityBox.appendChild(imgIcon);
       // create current temp
       var temp = document.createElement("p");
       temp.textContent = `\n` + `Temp:${data.main.temp}`;
@@ -86,40 +93,69 @@ $(".form").on("submit", function () {
   }
 });
 
-// fetchButton.addEventListener("click", fetchCurrent);
-
 // weather cards
 function fetchForecast(lat, lon) {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}&units=imperial`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      var cardFiveDay = document.querySelectorAll(".card-fiveday");
+      var j = 0;
       for (var i = 0; i < data.list.length; i = i + 8) {
         console.log(data.list[i]);
+        cardFiveDay[j].innerHTML = "";
+        var currentDay = moment(data.list[i].dt_txt).format("M/D/YYYY");
+        console.log(currentDay);
+        var day = document.createElement("h2");
+        day.textContent = currentDay;
+        cardFiveDay[j].appendChild(day);
+        var imgIcon = document.createElement("img");
+        imgIcon.setAttribute(
+          "src",
+          `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`
+        );
+        cardFiveDay[j].appendChild(imgIcon);
         var temp = document.createElement("p");
-        temp.textContent = `Temp:${data.main.temp}`;
-        cityBox.appendChild(temp);
+        temp.textContent = `Temp:${data.list[i].main.temp}`;
+        cardFiveDay[j].appendChild(temp);
+        // create current day
+        // create current temp
+
+        // create current wind
+        var wind = document.createElement("p");
+        wind.textContent = `\n` + `Wind:${data.list[i].wind.speed}` + `MPH`;
+        cardFiveDay[j].appendChild(wind);
+        // create current humidity
+        var humid = document.createElement("p");
+        humid.textContent =
+          `\n` + `Humidity:${data.list[i].main.humidity}` + `%`;
+        cardFiveDay[j].appendChild(humid);
+        j++;
       }
     });
 }
 
 //local storage
-var searchList = function (cityName) {
+var searchList = function (savedCities) {
   // create entry with city name
   var searchHistoryEntry = $("<button>");
   var searchHistoryContainer = document.querySelector(
     ".search-history-container"
   );
+
   searchHistoryEntry.text(cityName);
+  console.log();
   searchHistoryContainer.append(searchHistoryEntry);
+
+  fetchButton.addEventListener("click", savedCities()); //click event
 
   if (savedSearches.length > 0) {
     // update savedSearches array with previously saved searches
-    var SavedSearches = localStorage.getItem("");
+    var savedSearches = localStorage.getItem("");
     savedSearches = JSON.parse();
   }
 
@@ -129,6 +165,4 @@ var searchList = function (cityName) {
 
   // reset search input
   $("#inputCity").val("");
-
-  searchList();
 };
